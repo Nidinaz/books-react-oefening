1bd2675efdf808c866488c448a25caace1f6733f
Dorina Kamp <dorina.kamp@gmail.com>
2022-11-03 16:37:06 +0100

    html table to div table

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const bookProperties = [
     {
-      ISBN: '978-1-61450-524-2',
-      Title: 'A Wizards Guide to Defensive Baking',
-      Author: 'KingFischer, T. ',
+      ISBN: "978-1-61450-524-2",
+      Title: "A Wizards Guide to Defensive Baking",
+      Author: "KingFischer, T. ",
       Publishingyear: 2020,
       FirstPublished: 2020,
-      Genre: 'Fantasy',
-      Language: 'English',
-      originalLanguage: 'English',
-      Publisher: 'Argyll Productions',
-      Pages: 320
+      Genre: "Fantasy",
+      Language: "English",
+      originalLanguage: "English",
+      Publisher: "Argyll Productions",
+      Pages: 320,
     },
 
     {
-      ISBN: '978-3-7175-2422-9',
-      Title: 'Das Babylonische Woerterbuch',
-      Author: 'Machado de Assis, Joaquim Maria',
+      ISBN: "978-3-7175-2422-9",
+      Title: "Das Babylonische Woerterbuch",
+      Author: "Machado de Assis, Joaquim Maria",
       Publishingyear: 2018,
-      FirstPublished: 'not known',
-      Genre: 'Stories',
-      Language: 'English',
-      originalLanguage: 'Brazilian',
-      Publisher: 'Manesse Verlag',
-      Pages: 256
-    }
-  ]
-
+      FirstPublished: "not known",
+      Genre: "Stories",
+      Language: "English",
+      originalLanguage: "Brazilian",
+      Publisher: "Manesse Verlag",
+      Pages: 256,
+    },
+  ];
 
   return (
-  <div>
-    <Header></Header>
-    <Book book={bookProperties[0]}></Book>
-    <Book book={bookProperties[1]}></Book>
-  </div>
+    <div>
+      <Header></Header>
+      <Book book={bookProperties[0]}></Book>
+      <Book book={bookProperties[1]}></Book>
+    </div>
   );
 }
 
@@ -44,76 +42,55 @@ function App() {
 const Book = (props) => {
   console.log(props);
   return (
-    <div>
-      <table>
-        <thead>
-          <th colspan='2'></th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>ISBN</td>
-            <td>{props.book.ISBN}</td>
-          </tr>
-          <tr>
-            <td>Title</td>
-            <td>{props.book.Title}</td>
-          </tr>
-          <tr>
-            <td>Publishing year</td>
-            <td>{props.book.Publishingyear}</td>
-          </tr>
-          <tr>
-            <td>First published</td>
-            <td>{props.book.FirstPublished}</td>
-          </tr>
-          <tr>
-            <td>Genre</td>
-            <td>{props.book.Genre}</td>
-          </tr>
-          <tr>
-            <td>Language</td>
-            <td>{props.book.Language}</td>
-          </tr>
-          <tr>
-            <td>original Language</td>
-            <td>{props.book.originalLanguage}</td>
-          </tr>
-          <tr>
-            <td>Publisher</td>
-            <td>{props.book.Publisher}</td>
-          </tr>
-          <tr>
-            <td>Pages</td>
-            <td>{props.book.Pages}</td>
-          </tr>
-        </tbody>
-      </table>
-      {/* <p>{props.book.ISBN}</p>
-      <p>{props.book.Title}</p>
-      <p>{props.book.Author}</p>
-      <p>{props.book.Publishingyear}</p>
-      <p>{props.book.FirstPublished}</p>
-      <p>{props.book.Genre}</p>
-      <p>{props.book.Language}</p>
-      <p>{props.book.originalLanguage}</p>
-      <p>{props.book.Publisher}</p>
-      <p>{props.book.Pages}</p> */}
+    <div className="divTable">
+      <div className="tableBody">
+        <div className="tableRow">
+          <div className="tableCell">ISBN</div>
+          <div className="tableCell">{props.book.ISBN}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">Title</div>
+          <div className="tableCell">{props.book.Title}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">Publishing year</div>
+          <div className="tableCell">{props.book.Publishingyear}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">First published</div>
+          <div className="tableCell">{props.book.FirstPublished}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">Genre</div>
+          <div className="tableCell">{props.book.Genre}</div>
+          <div className="tableRow"></div>
+          <div className="tableCell">Language</div>
+          <div className="tableCell">{props.book.Language}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">original Language</div>
+          <div className="tableCell">{props.book.originalLanguage}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">Publisher</div>
+          <div className="tableCell">{props.book.Publisher}</div>
+        </div>
+        <div className="tableRow">
+          <div className="tableCell">Pages</div>
+          <div className="tableCell">{props.book.Pages}</div>
+        </div>
+      </div>
     </div>
   );
 };
-  
 
-  const Header = () => {
-
-      return (
-        <div>
-          <h1>Books</h1>
-          <h2>
-            a list of books I happen to like
-          </h2>
-        </div>
+const Header = () => {
+  return (
+    <div>
+      <h1>Books</h1>
+      <h2>a list of books I happen to like</h2>
+    </div>
   );
 };
-
 
 export default App;
